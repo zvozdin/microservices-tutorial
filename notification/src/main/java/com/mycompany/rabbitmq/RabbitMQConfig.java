ed9ed08cf0dde175bebd2b8435a21ecfe68ab825
Oleksandr Zvozdin <zvyozdin@gmail.com>
2022-01-25 15:18:10 +0200

    docker compose entire app

--- notification/src/main/java/com/mycompany/rabbitmq/RabbitMQConfig.java
+++ notification/src/main/java/com/mycompany/rabbitmq/RabbitMQConfig.java
@@ -1,12 +1,22 @@
 package com.mycompany.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.my.queue}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
